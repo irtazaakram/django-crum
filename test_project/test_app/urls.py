59de1e0d9cdc4c93d1ce0a2479be67048a222cf6
irtazaakram <irtaza.akram@arbisoft.com>
2023-07-03 11:37:53 +0500

    PR ready

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -1,15 +1,16 @@
 # Django
 try:
-    from django.urls import path
+    from django.urls import re_path
 except ImportError:
-    from django.conf.urls import url as path
+    from django.conf.urls import url as re_path
 
 # Test App
-from test_project.test_app.views import api_index, index
+from test_project.test_app.views import index, api_index
+
 
 app_name = 'test_app'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('api/', api_index, name='api_index'),
+    re_path(r'^$', index, name='index'),
+    re_path(r'^api/$', api_index, name='api_index'),
 ]
--- test_project/urls.py
+++ test_project/urls.py
@@ -2,21 +2,21 @@
 from django.conf import settings
 
 try:
-    from django.urls import include, path
+    from django.urls import include, re_path
 except ImportError:
-    from django.conf.urls import url as path
+    from django.conf.urls import include, url as re_path
 
 include_kwargs = dict(namespace='test_app')
 
 urlpatterns = [
-    path('test_app/', include('test_project.test_app.urls', **include_kwargs)),
+    re_path(r'^test_app/', include('test_project.test_app.urls', **include_kwargs)),
 ]
 
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
     from django.contrib import admin
     admin.autodiscover()
     urlpatterns += [
-        path('', admin.site.urls),
+        re_path(r'', admin.site.urls),
     ]
 
 if 'django.contrib.staticfiles' in settings.INSTALLED_APPS and settings.DEBUG:
